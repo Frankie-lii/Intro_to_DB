23bf56d1bee05f36e8108651665bca11e2824d80
Frankie-lii <franklondon89@yahoo.com>
2025-10-17 00:22:58 +0300

    Add multiple customer records to customer table
    Inserts multiple customer records into the database.

---
+++ task_6.sql
@@ -0,0 +1,9 @@
+-- Script: task_6.sql
+-- Inserts multiple rows into the 'customer' table of the given database
+-- Usage: cat task_6.sql | mysql -u root -p alx_book_store
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
