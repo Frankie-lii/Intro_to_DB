08cd7b4e019eb88cfd4dd86847c08a26ee8d2dd3
Frankie-lii <franklondon89@yahoo.com>
2025-10-17 00:28:20 +0300

    Add comments and usage instructions to task_3.sql
    Updated script comments and usage instructions.

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,5 @@
-mysql -u username -p alx_book_store < task_3.sql
+-- Script: task_3.sql
+-- Lists all the tables in the given database
+-- Usage: cat task_3.sql | mysql -u root -p alx_book_store
+
+SHOW TABLES;
