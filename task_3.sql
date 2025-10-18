73c58ea4ebb49a71b3b31f998a2a14d660030d77
Frankie-lii <franklondon89@yahoo.com>
2025-10-18 12:06:34 +0300

    Add usage comment for listing tables in SQL script

--- task_3.sql
+++ task_3.sql
@@ -1,5 +1,2 @@
--- Script: task_3.sql
--- Lists all the tables in the given database
--- Usage: cat task_3.sql | mysql -u root -p alx_book_store
-
+-- List all tables in the given database
 SHOW TABLES;
